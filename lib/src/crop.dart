e18d8d5dee3a879a6abe79e8efdde14a0a261cd2
koo <koomingchin@gmail.com>
2023-06-18 14:49:14 +0800

    top padding

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -450,7 +450,7 @@ class _CropEditorState extends State<_CropEditor> {
         _targetImage!,
         Rect.fromLTWH(
           rectLeftPx > padding ? rectLeftPx - padding : rectLeftPx,
-          rectTopPx,
+          rectTopPx > padding ? rectTopPx - padding : rectTopPx,
           (_rect.width) * screenSizeRatio / _scale,
           (_rect.height) * screenSizeRatio / _scale,
         ),
