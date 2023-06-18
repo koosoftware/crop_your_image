0d035a0cb5fa57dc7d227b00dfee626d9cb2ec11
koo <koomingchin@gmail.com>
2023-06-18 18:23:19 +0800

    double padding

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -435,7 +435,7 @@ class _CropEditorState extends State<_CropEditor> {
 
     widget.onStatusChanged?.call(CropStatus.cropping);
 
-    double padding = widget.padding + 2;
+    double padding = widget.padding * 2;
     double paddingPx = padding * screenSizeRatio;
 
     double rectLeft = _rect.left - _imageRect.left;
