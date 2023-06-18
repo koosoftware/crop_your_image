6c65bfed9b91018bdb817287c21cba5d146aa495
koo <koomingchin@gmail.com>
2023-06-18 15:58:13 +0800

    fourth

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -438,8 +438,8 @@ class _CropEditorState extends State<_CropEditor> {
     double padding = widget.padding;
     double paddingPx = padding * screenSizeRatio;
 
-    double rectLeft = _rect.left - _imageRect.left - paddingPx;
-    double rectTop = _rect.top - _imageRect.top - paddingPx;
+    double rectLeft = _rect.left - _imageRect.left - padding;
+    double rectTop = _rect.top - _imageRect.top - padding;
 
     double rectLeftPx = rectLeft * screenSizeRatio / _scale;
     double rectTopPx = rectTop * screenSizeRatio / _scale;
