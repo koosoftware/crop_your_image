2801ab08d469450222f1ff8e1659d768b6658cbd
koo <koomingchin@gmail.com>
2023-06-19 20:05:21 +0800

    top

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -447,7 +447,7 @@ class _CropEditorState extends State<_CropEditor> {
         _targetImage!,
         Rect.fromLTWH(
           rectLeftPx > paddingPx ? rectLeftPx - paddingPx : rectLeftPx,
-          rectTopPx > paddingPx ? rectTopPx - paddingPx : rectTopPx,
+          rectTopPx - paddingPx,
           (rectLeftPx > paddingPx ? _rect.width : _rect.width - paddingPx) *
               screenSizeRatio /
               _scale,
