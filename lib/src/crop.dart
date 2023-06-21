69d0b0f71b7a3c42dfb4db2a61011be55d588f9b
koo <koomingchin@gmail.com>
2023-06-22 07:22:48 +0800

    init scale 1

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -287,8 +287,7 @@ class _CropEditorState extends State<_CropEditor> {
   }) {
     late double baseHeight;
     late double baseWidth;
-    final ratio = (_targetImage!.height + widget.padding * 2) /
-        (_targetImage!.width + widget.padding * 2);
+    final ratio = _targetImage!.height / _targetImage!.width;
 
     if (_isFitVertically) {
       baseHeight = MediaQuery.of(context).size.height;
@@ -417,7 +416,7 @@ class _CropEditorState extends State<_CropEditor> {
 
     if (widget.interactive) {
       final initialScale = calculator.scaleToCover(screenSize, _imageRect);
-      _applyScale(initialScale);
+      _applyScale(1);
     }
   }
 
