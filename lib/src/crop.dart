4a41ff357147a21fd112ccbe5983597c2bcd9aae
koo <koomingchin@gmail.com>
2023-06-21 12:03:25 +0800

    five

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -252,7 +252,7 @@ class _CropEditorState extends State<_CropEditor> {
     // move
     double newImageRectWidth =
         _imageRect.width - 200 / (_baseScale * detail.scale);
-    double widthDiff = (newImageRectWidth - _imageRect.width - 200).abs();
+    double widthDiff = (newImageRectWidth - _imageRect.width).abs();
     double finalNewImageRectWidth = newImageRectWidth + widthDiff;
 
     var movedLeft = _imageRect.left + detail.focalPointDelta.dx;
