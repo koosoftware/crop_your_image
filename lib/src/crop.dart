7962a4eee6746869e37a581341124fcf545d3933
koo <koomingchin@gmail.com>
2023-06-22 17:38:12 +0800

    one

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -531,11 +531,13 @@ class _CropEditorState extends State<_CropEditor> {
                               widget.image,
                               width: _isFitVertically
                                   ? null
-                                  : MediaQuery.of(context).size.width -
-                                      widget.padding * 2 * _scale,
+                                  : (MediaQuery.of(context).size.width -
+                                          widget.padding * 2) *
+                                      _scale,
                               height: _isFitVertically
-                                  ? MediaQuery.of(context).size.height -
-                                      widget.padding * 2 * _scale
+                                  ? (MediaQuery.of(context).size.height -
+                                          widget.padding * 2) *
+                                      _scale
                                   : null,
                               fit: BoxFit.contain,
                             ),
