76904a178965d703af3ab2579a667b82be220bc0
koo <koomingchin@gmail.com>
2023-06-25 18:46:20 +0800

    testt

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -315,9 +315,9 @@ class _CropEditorState extends State<_CropEditor> {
 
     // position
     final newLeft = max(min(_rect.left, _imageRect.left - leftPositionDelta),
-        _rect.right - newWidth);
+        _rect.right - newWidth + widget.padding);
     final newTop = max(min(_rect.top, _imageRect.top - topPositionDelta),
-        _rect.bottom - newHeight);
+        _rect.bottom - newHeight + widget.padding);
 
     if (newWidth < _rect.width || newHeight < _rect.height) {
       return;
