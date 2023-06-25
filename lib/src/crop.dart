635a573189961ad3ae9cb5572522eb42139a925a
koo <koomingchin@gmail.com>
2023-06-25 14:16:15 +0800

    revert

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -422,9 +422,8 @@ class _CropEditorState extends State<_CropEditor> {
       final initialScale = calculator.scaleToCover(
         screenSize,
         _imageRect,
-        widget.padding,
       );
-      _applyScale(initialScale);
+      _applyScale(1);
     }
   }
 
