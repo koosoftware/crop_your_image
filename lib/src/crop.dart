bca463ab71e818423f09fd9c280cc44ef435731a
koo <koomingchin@gmail.com>
2023-06-25 18:32:50 +0800

    why

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -298,7 +298,7 @@ class _CropEditorState extends State<_CropEditor> {
     }
 
     // width
-    final newWidth = (baseWidth + widget.padding) * nextScale;
+    final newWidth = (baseWidth + widget.padding * 2) * nextScale;
     final horizontalFocalPointBias = focalPoint == null
         ? 0.5
         : (focalPoint.dx - _imageRect.left) / _imageRect.width;
@@ -306,7 +306,7 @@ class _CropEditorState extends State<_CropEditor> {
         (newWidth - _imageRect.width) * horizontalFocalPointBias;
 
     // height
-    final newHeight = (baseHeight + widget.padding) * nextScale;
+    final newHeight = (baseHeight + widget.padding * 2) * nextScale;
     final verticalFocalPointBias = focalPoint == null
         ? 0.5
         : (focalPoint.dy - _imageRect.top) / _imageRect.height;
@@ -325,7 +325,7 @@ class _CropEditorState extends State<_CropEditor> {
     // apply
     setState(() {
       _imageRect = Rect.fromLTRB(
-        newLeft + (widget.padding * nextScale / 2),
+        newLeft,
         newTop,
         newLeft + newWidth,
         newTop + newHeight,
